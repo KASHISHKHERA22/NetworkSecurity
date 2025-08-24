4517edd6fedafb87876f02adea2719a940f17da9
Kashish Khera <kashishkhera20@gmail.com>
2025-08-25 00:51:45 +0530

    Model-confid Updated

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -41,3 +41,11 @@ class DataTransformationConfig:
         self.transformed_test_file_path = os.path.join(self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,training_pipeline.TEST_FILE_NAME.replace("csv","np"))
 
         self.transformed_object_file_path = os.path.join(self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,training_pipeline.PREPROCESSING_OBJECT_FILE_NAME )
+
+class ModelTrainerConfig:
+    def __init(self,training_pipeline_config: TrainingPipelineConfig):
+        self.model_trainer_dir = os.path.join(training_pipeline_config.artifact_dir,training_pipeline.MODEL_TRAINER_DIR_NAME)
+        self.trained_model_file_path = os.path.join(self.model_trainer_dir,training_pipeline.MODEL_TRAINER_TRAINED_MODEL_DIR)
+        self.expected_score:float = training_pipeline.MODEL_TRAINER_EXPECTED_SCORE
+        self.overfitting_threshold:float = training_pipeline.MODEL_TRAINER_OVERFITTING_UNDERFITTING_THRESHOLD
+    
